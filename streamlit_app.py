b87ddca0028bc1af8edbb7e3a7bcea0c359a9d93
analazovic97 <56603734+analazovic97@users.noreply.github.com>
2023-06-09 15:20:47 +0200

    Update streamlit_app.py
    reverse order

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,10 +26,12 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 #streamlit.text(fruityvice_response.json())
 
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 # write your own comment - normalize json from api response
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - from json to dataframe
 streamlit.dataframe(fruityvice_normalized)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+
