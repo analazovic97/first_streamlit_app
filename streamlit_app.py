6664c670976625036ced4dcda45a036d5cc49e0f
analazovic97 <56603734+analazovic97@users.noreply.github.com>
2023-06-10 19:59:51 +0200

    Update streamlit_app.py
    fixing error

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,7 +72,7 @@ streamlit.stop() #Don't run anything past here while troubleshooting
 #Allow end user to add fruit to the list
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-        my_cur.execute("insert into fruit_load_list values (" + ???? + ")")
+        my_cur.execute("insert into fruit_load_list values ("' + ???? + '")")
         return "Thanks for adding " + new_fruit
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
